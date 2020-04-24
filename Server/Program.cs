2f38bd5d99ae9c9bbe48dcbd7d6b7d0e42fc2060
davlveek <stankeev.vladimir@gmail.com>
2020-04-24 15:48:57 +0300

    Connect client

--- Server/Program.cs
+++ Server/Program.cs
@@ -4,13 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Sockets;
+
 namespace Server
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var port = Convert.ToInt32(args[0]);
 
+            TcpListener server = null;
+            try
+            {
+                IPAddress addr = IPAddress.Parse("127.0.0.1");
+                server = new TcpListener(addr, port);
+                server.Start();
+                Console.WriteLine("Server started. Waiting for connections.");
+
+                TcpClient client = server.AcceptTcpClient();
+                var clientAddress = Convert.ToString(((IPEndPoint)client.Client.RemoteEndPoint).Address);
+                Console.WriteLine("Client connected");
+
+                while (true)
+                {
+                    
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.Stop();
+                }
+            }
         }
     }
 }
