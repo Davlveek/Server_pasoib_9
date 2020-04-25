86f86e7a852ba7ce93a56b6e8202016246fef592
davlveek <stankeev.vladimir@gmail.com>
2020-04-25 21:15:27 +0300

    Done server

--- Server/CryptoActions.cs
+++ Server/CryptoActions.cs
@@ -1,15 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-using CryptoPro.Sharpei;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
 
 namespace Server
 {
     class CryptoActions
     {
-        
+        static public bool VerifyMsg(byte[] data, out byte[] msg)
+        {
+            var cms = new SignedCms();
+            cms.Decode(data);
+
+            try
+            {
+                cms.CheckSignature(true);
+            }
+            catch (CryptographicException)
+            {
+                msg = null;
+                return false;
+            }
+
+            msg = cms.ContentInfo.Content;
+
+            return true;
+        }
+
+        static public byte[] Decrypt(byte[] data)
+        {
+            var cms = new EnvelopedCms();
+            cms.Decode(data);
+
+            Console.WriteLine($"\tEncryption algorithm: {cms.ContentEncryptionAlgorithm.Oid.FriendlyName}");
+
+            cms.Decrypt(cms.RecipientInfos[0]);
+            return cms.ContentInfo.Content;
+        }
+
+        static public bool VerifySign(byte[] data)
+        {
+            var cms = new SignedCms();
+            cms.Decode(data);
+
+            try
+            {
+                cms.CheckSignature(true);
+                Console.WriteLine("\tSign status: correct.");
+
+                ParseData(cms);
+            }
+            catch (CryptographicException)
+            {
+                Console.WriteLine("\tSign status: incorrect");
+                return false;
+            }
+
+            return true;
+        }
+
+        static public void ParseData(SignedCms cms)
+        {
+            var info = cms.SignerInfos[0];
+            var certificate = info.Certificate;
+
+            Console.WriteLine("\tCertificate");
+            Console.WriteLine($"\tSubject: {certificate.Subject}");
+            Console.WriteLine($"\tIssuer: {certificate.Issuer}");
+            Console.WriteLine($"\tValid from: {certificate.NotBefore}");
+            Console.WriteLine($"\tValid on: {certificate.NotAfter}");
+            Console.WriteLine($"\tSerial number: {certificate.SerialNumber}");
+            Console.WriteLine($"\tSign algorithm: {certificate.PublicKey.Oid.FriendlyName}");
+
+            Console.WriteLine(Environment.NewLine);
+
+            for (int i = 0; i < cms.SignerInfos[0].SignedAttributes.Count; i++)
+            {
+                if (cms.SignerInfos[0].SignedAttributes[i].Values[0] is Pkcs9SigningTime signingTime)
+                {
+                    Console.WriteLine($"\tSigning time: {signingTime.SigningTime}");
+                }
+            }
+
+            var msg = cms.ContentInfo.Content;
+            Console.WriteLine($"\tMessage: {new UnicodeEncoding().GetString(msg)}");
+        }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 using System.Net;
 using System.Net.Sockets;
 
@@ -18,20 +14,65 @@ namespace Server
             TcpListener server = null;
             try
             {
-                IPAddress addr = IPAddress.Parse("127.0.0.1");
+                var addr = IPAddress.Parse("127.0.0.1");
                 server = new TcpListener(addr, port);
                 server.Start();
-                Console.WriteLine("Server started. Waiting for connections.");
-
-                TcpClient client = server.AcceptTcpClient();
-                var clientAddress = Convert.ToString(((IPEndPoint)client.Client.RemoteEndPoint).Address);
-                Console.WriteLine("Client connected");
+                Console.WriteLine("[*] Server started. Waiting for connections.");
 
                 while (true)
                 {
-                    
+                    TcpClient client = server.AcceptTcpClient();
+                    var clientAddress = Convert.ToString(((IPEndPoint)client.Client.RemoteEndPoint).Address);
+                    Console.WriteLine($"[*] Client connected: {clientAddress}");
 
+                    NetworkStream stream = client.GetStream();
 
+                    byte[] data = new byte[2048];
+
+                    while (true)
+                    {
+                        var closeFlag = false;
+
+                        stream.Read(data, 0, data.Length);
+                        var length = new UnicodeEncoding().GetString(data);
+                        length = length.Substring(0, 1);
+
+                        stream.Read(data, 0, data.Length);
+                        string cmd = new UnicodeEncoding().GetString(data);
+                        cmd = cmd.Substring(0, Convert.ToInt32(length));
+
+                        switch (cmd)
+                        {
+                            case "Sign":
+                                Console.WriteLine(Environment.NewLine, "Sign action");
+                                stream.Read(data, 0, data.Length);
+                                CryptoActions.VerifySign(data);
+                                break;
+
+                            case "Encrypt":
+                                Console.WriteLine(Environment.NewLine, "Encrypt Action");
+                                stream.Read(data, 0, data.Length);
+                                byte[] decryptedData = CryptoActions.Decrypt(data);
+                                CryptoActions.VerifyMsg(decryptedData, out byte[] msg);
+                                Console.WriteLine($"\tDecrpyted message: {new UnicodeEncoding().GetString(msg)}");
+                                break;
+
+                            case "Close":
+                                stream.Close();
+                                client.Close();
+                                Console.WriteLine($"[*] Client disconnected: {clientAddress}");
+                                closeFlag = true;
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        if (closeFlag)
+                        {
+                            break;
+                        }
+                    }
                 }
             }
             catch (Exception e)
